f5dc1fc6366d8474298da076584ea0c40cdf2f33
annisgood <98206319+annisgood@users.noreply.github.com>
2022-01-29 17:38:42 +0800

    add new

--- products.py
+++ products.py
@@ -1,14 +1,31 @@
+import os
+
 products = []
+if os.path.isfile('products.csv'):
+	print('yeah')
+	with open ('products.csv' , 'r', encoding= 'utf-8' ) as f:
+		for line in f :
+			if '商品,價格' in line:
+				continue
+		name ,price = line.strip().split(',')
+		products.append([name,price])
+	print(products)
+else:
+	print('找不到')
+
 while True:
     name = input('請輸入商品名稱:')
     if name == 'q':
         break
     price = input('請輸入價格:') 
-    p = []
-    p.append(name)
-    p.append(price)
-    products.append(p)
+    price = int(price)
+    products.append([name, price])
 print(products)
 
 for p in products:
 	print(p[0],'的價格是', p[1])
+
+with open ('products.csv', 'w' , encoding= 'utf-8') as f :
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' +p[1] + '\n')
